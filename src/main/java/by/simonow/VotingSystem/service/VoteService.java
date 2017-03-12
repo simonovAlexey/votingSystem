bc8eda5495d930e5c2dfe2638a9c22e8958a07af
Simonov <3694369@gmail.com>
2017-03-13 00:24:07 +0300

    add VoteService

---
+++ src/main/java/by/simonow/VotingSystem/service/VoteService.java
@@ -0,0 +1,26 @@
+package by.simonow.VotingSystem.service;
+
+
+import by.simonow.VotingSystem.model.Votes;
+import by.simonow.VotingSystem.util.exception.NotFoundException;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface VoteService {
+
+    Votes get(int id, int userId) throws NotFoundException;
+
+    void delete(int id, int userId) throws NotFoundException;
+
+    List<Votes> getAll(int userId);
+
+    List<Votes> getAllByUser(int userId);
+
+    Votes update(Votes votes, int userId) throws NotFoundException;
+
+    Votes save(Votes votes, int userId);
+
+    Votes getTodayVote(LocalDateTime startDate, LocalDateTime endDate, int userId);
+
+}
